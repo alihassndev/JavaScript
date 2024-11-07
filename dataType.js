f62946983e92b24db2b310a75d74d057c3348d97
Ali Hassan <alihassndev@gmail.com>
2024-11-07 13:53:34 +0500

    complete datatype

---
+++ dataType.js
@@ -0,0 +1,19 @@
+"use strict"; // treated as newer version of js
+
+// make this sure that code must be readable
+
+// we do not use alert commands at server side as it is browser side command
+// alert("Hey there!");
+
+// number => {2, 2.23, 2e3, etc}
+// bigint => {1235568996543245}
+// String => {"Ali", "Hassan"}
+// Boolean => {true | false}
+// null => {stand alone value} => This is an object
+// undefined=> {un-specified} => This is data type
+// symbols => {unique}
+
+// Object
+
+console.log(typeof null);
+console.log(typeof undefined);
