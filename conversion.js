3a3b273824184c1c0bd52c83ce5c8e35f672872e
Ali Hassan <alihassndev@gmail.com>
2024-11-07 21:14:05 +0500

    conversion of dataTypes

---
+++ conversion.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// let age = null;
+// let age = undefined;
+// let age = "22ab";
+// console.log(`Type of age : ${typeof age}`);
+// console.log(`Value of age : ${age}`);
+
+// let valueNumber = Number(age);
+// console.log(`Type of age : ${typeof valueNumber}`);
+// console.log(`Value of age : ${valueNumber}`);
+// alternative method of above
+// console.log(`Type of age : ${typeof age}`);
+
+// ==============
+
+// 0 & "" => false
+// != 0 & "anything" => true
+
+// ==============
+
+// let isloggedIn = "";
+let isloggedIn = 1;
+let booleanlogin = Boolean(isloggedIn);
+// console.log(`Type of age : ${typeof booleanlogin}`);
+// console.log(`Value of age : ${booleanlogin}`);
+
+let someNumber = 3;
+let stringNum = String(someNumber);
+
+console.log(typeof stringNum);
+console.log(stringNum);
