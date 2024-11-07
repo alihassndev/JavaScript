89eebb23cb4a8982d44cffba56108875e05add23
Ali Hassan <alihassndev@gmail.com>
2024-11-07 21:51:46 +0500

    working with comparison operators

---
+++ comparison.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+
+// console.log("2" == 2);
+
+// console.log(null == 0); // False
+// console.log(Number(null) == 0); // true
+// console.log(null >= 0); // true
+// console.log(null > 0); // false
+// console.log(null < 0); // false
+// console.log(null <= 0); // true
+
+// console.log(undefined == 0); // false
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false
