9bfe453c93661768d5904d83e7eae63496c4e7b7
Ali Hassan <alihassndev@gmail.com>
2024-11-07 13:39:50 +0500

    complete first.js

--- first.js
+++ first.js
@@ -1,3 +1,15 @@
-let name = "Ali";
+"use strict";
 
-console.log(name);
+let name = "Ali";
+let age = 22;
+let city = "Sialkot";
+let degree = "BCS";
+let state;
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(degree);
+// console.log(state);
+
+console.table([name, age, city, degree, state]);
