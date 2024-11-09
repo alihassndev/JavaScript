ad4a337542044468d247dd7320f62a155678c5f6
Ali Hassan <alihassndev@gmail.com>
2024-11-09 19:43:49 +0500

    start working with Strings

---
+++ strings.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const name = "Ali";
+const repo = 50;
+
+// console.log(name + repo + " value");
+
+// String Interpolations
+// console.log(`Hello my name is ${name}, and my repo count is ${repo}`);
+
+// Alternate to declare a string
+const gameName = new String("ali"); // String is an object
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName);
+// console.log(gameName.charAt(2)); // => output : 'i'
+console.log(gameName.indexOf("l")); // => output : 'i'
+
+// console.log(gameName.__proto__); // returns an object
