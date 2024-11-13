4ed621957f4062b8ed6802738e45920dd5681072
Ali Hassan <alihassndev@gmail.com>
2024-11-13 09:34:43 +0500

    object part 2 completed

--- objects.js
+++ objects.js
@@ -43,6 +43,64 @@ user.greeting2 = function () {
   console.log(`Hello JS user, ${this["full name"]}`);
 };
 
-console.log(user);
-user.greeting();
-user.greeting2();
+// console.log(user);
+// user.greeting();
+// user.greeting2();
+
+// ================== Part 2 ==================
+// const olxUser = new Object();
+const olxUser = {};
+olxUser.id = "123";
+olxUser.name = "Ali";
+olxUser.isLoggedIn = false;
+
+// console.log(olxUser);
+
+const regularUser = {
+  email: "ali@gmail.com",
+  fullName: {
+    userFullName: {
+      firstName: "Ali",
+      lastName: "Hassan",
+    },
+  },
+};
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = { obj1, obj2 };
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+// const obj3 = Object.assign(obj1, obj2);
+
+const obj3 = { ...obj1, ...obj2, ...obj4 };
+// console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    email: "ali@gmail.com",
+  },
+  {
+    id: 2,
+    email: "ali@gmail.com",
+  },
+  {
+    id: 3,
+    email: "ali@gmail.com",
+  },
+];
+
+// user[1].email;
+console.log(olxUser);
+
+console.log(Object.keys(olxUser));
+console.log(Object.values(olxUser));
+// console.log(Object.entries(olxUser));
+
+console.log(olxUser.hasOwnProperty("isLoggedIn")); // output => true
+console.log(olxUser.hasOwnProperty("isLogged")); // output => false
