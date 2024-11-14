27f2741893bfa53447d19b2394971152c472a7f4
Ali Hassan <alihassndev@gmail.com>
2024-11-14 10:56:35 +0500

    arrow functions

--- arrowFunction.js
+++ arrowFunction.js
@@ -16,9 +16,41 @@ const user = {
 // user.welcomeMessage();
 // console.log(this);
 
-function chai() {
-  let username = "ali";
-  console.log(this.username);
-}
+// function chai() {
+//   let username = "ali";
+//   console.log(this.username);
+// }
 
-chai();
+// chai();
+
+// const coffee = function () {
+//   const username = "Ali";
+//   console.log(this.username); // Output => undefined
+// };
+
+// coffee();
+
+// const coffee = () => {
+//   const username = "Ali";
+//   console.log(this.username); // Output => undefined
+// };
+
+// coffee();
+
+// const addNums = (num1, num2) => {
+// return num1 + num2; // Explicit return
+// };
+
+// Implicit Return
+// const addNums = (num1, num2) => num1 + num2;
+
+const addNums = (num1, num2) => ({ username: "Ali" });
+
+console.log(addNums(3, 5));
+
+// Array Iteration
+// const myArray = [1, 5, 6, 3, 2];
+
+// myArray.forEach((element) => {
+//   console.log(element);
+// });
